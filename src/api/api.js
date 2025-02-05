5f353008436ddacdac5f71430a2fe80c3c42f76f
Sedenu <aloayecharles@gmail.com>
2025-02-05 12:30:57 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://api.schoolcafe.ng", //API URL
+  // baseURL: "https://api.schoolcafe.ng", //THIS API URL CAN BE SORTED LATER
+  baseURL: "https://afe-52zn.onrender.com/", // USE THIS API URL FOR NOW
   headers: {
     "Content-Type": "application/json",
-    "Accept": "application/json",
+    Accept: "application/json",
   },
   withCredentials: true, // cookies if authentication requires it
 });
