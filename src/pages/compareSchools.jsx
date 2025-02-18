fcda2fa3522ff29ea254f9a97842c07d1da55779
abed obaah <ubahobaah@gmail.com>
2025-02-18 15:28:47 +0100

    dashboard with sponsored adds

--- src/pages/compareSchools.jsx
+++ src/pages/compareSchools.jsx
@@ -9,7 +9,7 @@ const navigation = [
   { name: 'Product', href: '#' },
   { name: 'Features', href: '#' },
   { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Companys', href: '#' },
 ]
 
 export default function compareSchools() {
